1c98ed30bbe64e1c185385f83f1f978f64ab896f
Lukas Fittl <lukas@fittl.com>
2015-08-09 01:29:23 -0700

    Use libpg_query for Postgres parser access.

--- parse.go
+++ parse.go
@@ -4,10 +4,11 @@ package pg_query_go
 // https://github.com/golang/go/commit/131758183f7dc2610af489da3a7fcc4d30c6bc48
 
 /*
-#cgo CFLAGS: -I${SRCDIR} -I ${SRCDIR}/postgres/src/include
-#cgo LDFLAGS: -L${SRCDIR} -lpg_query -fstack-protector
+#cgo CFLAGS: -I${SRCDIR}/tmp/libpg_query-master
+#cgo LDFLAGS: -L${SRCDIR}/tmp/libpg_query-master -lpg_query -fstack-protector
 #cgo LDFLAGS: -Wl,-undefined,dynamic_lookup -Wl,-multiply_defined,suppress
 #include <pg_query.h>
+#include <stdlib.h>
 */
 import "C"
 
@@ -19,9 +20,11 @@ func Parse(input string) string {
   input_c := C.CString(input)
   defer C.free(unsafe.Pointer(input_c))
 
-  result_c := C.pg_query_raw_parse(input_c)
+  result_c := C.pg_query_parse(input_c)
+  defer C.free(unsafe.Pointer(result_c.parse_tree))
+  defer C.free(unsafe.Pointer(result_c.stderr_buffer))
 
-  result := C.GoString(result_c)
+  result := C.GoString(result_c.parse_tree)
 
   return result
 }
--- pg_query.c
+++
@@ -1,8 +0,0 @@
-#include "pg_query.h"
-
-const char* progname = "pg_query";
-
-void pg_query_init(void)
-{
-	MemoryContextInit();
-}
