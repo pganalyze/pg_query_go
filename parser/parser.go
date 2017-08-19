09c57ae8cd12b9337a88c200f7b8b624b9386cff
Bartosz Ocytko <bocytko@gmail.com>
2017-08-19 18:32:09 +0200

    Add support for PL/pgSQL parsing.

--- parser/parser.go
+++ parser/parser.go
@@ -37,6 +37,26 @@ func ParseToJSON(input string) (result string, err error) {
 	return
 }
 
+// ParsePlPgSqlToJSON - Parses the given PL/pgSQL function statement into an AST (JSON format)
+func ParsePlPgSqlToJSON(input string) (result string, err error) {
+	inputC := C.CString(input)
+	defer C.free(unsafe.Pointer(inputC))
+
+	resultC := C.pg_query_parse_plpgsql(inputC)
+
+	defer C.pg_query_free_plpgsql_parse_result(resultC)
+
+	if resultC.error != nil {
+		errMessage := C.GoString(resultC.error.message)
+		err = errors.New(errMessage)
+		return
+	}
+
+	result = C.GoString(resultC.plpgsql_funcs)
+
+	return
+}
+
 // Normalize the passed SQL statement to replace constant values with ? characters
 func Normalize(input string) (result string, err error) {
 	inputC := C.CString(input)
