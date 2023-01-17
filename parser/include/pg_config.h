17c3a0f7f50f4b681121730697b922842d417834
Benny Siegert <bsiegert@gmail.com>
2023-01-17 19:44:38 +0000

    pg_config.h: NetBSD support
    NetBSD has strchrnul too but with a slightly different prototype.
    Without this change, there is a compiler error for a conflicting
    declaration.

--- parser/include/pg_config.h
+++ parser/include/pg_config.h
@@ -990,6 +990,6 @@
 #undef HAVE_EXECINFO_H
 #undef HAVE_BACKTRACE_SYMBOLS
 #undef HAVE__GET_CPUID
-#ifdef __FreeBSD__
+#if defined(__FreeBSD__) || defined(__NetBSD__)
 #define HAVE_STRCHRNUL
 #endif
