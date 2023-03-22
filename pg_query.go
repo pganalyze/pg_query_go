6aa71c6b96f1321aa9d5fcdc60bf7eccfb824d26
Mokhtar Bacha <mokhtar@joinformal.com>
2023-03-22 19:51:33 +0100

    bumping to go.19

--- pg_query.go
+++ pg_query.go
@@ -3,7 +3,7 @@ package pg_query
 import (
 	proto "github.com/golang/protobuf/proto"
 
-	"github.com/pganalyze/pg_query_go/v4/parser"
+	"github.com/formalco/pg_query_go/v4/parser"
 )
 
 func Scan(input string) (result *ScanResult, err error) {
