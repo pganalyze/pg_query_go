d177e09d27061b50661dfbdb99ae126c1b713a7f
Gui Lin <guileen@gmail.com>
2025-11-26 19:20:53 +0800

    Fix xxhash symbol conflict by using namespace

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,7 @@
 package parser
 
 /*
-#cgo CFLAGS: -Iinclude -Iinclude/postgres -g -fstack-protector -std=gnu99 -Wno-unknown-warning-option -Wno-typedef-redefinition
+#cgo CFLAGS: -Iinclude -Iinclude/postgres -g -fstack-protector -std=gnu99 -Wno-unknown-warning-option -Wno-typedef-redefinition -DXXH_NAMESPACE=PG_QUERY_
 #cgo windows CFLAGS: -Iinclude/postgres/port/win32
 #cgo LDFLAGS:
 #include "pg_query.h"
